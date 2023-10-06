dac07a70a42f1ab0a6baa01f8bc4633b47d2eaf8
Babafemi Adesina <airchill101@gmail.com>
2023-10-05 22:10:54 -0700

    c basic

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - function
+ *
+ * Description: It calls puts and recieves the value returned
+ * Return: Return 0 to show succesful run
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
