1b75dc0fe6712f7f802eb09f73d4a663015f77c6
Babafemi Adesina <airchill101@gmail.com>
2023-10-09 16:08:36 -0700

    c basic

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
- * main - Entry point
+ * main - Prints last part of quote to standard error stream
  *
  * Return: Always 1 (Success)
  *
- *
+ * @void: parameter
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
 	return (1);
 }
