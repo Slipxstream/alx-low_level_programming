9743f9abc5bc630c0acfabe8d68f217557baf246
Babafemi Adesina <airchill101@gmail.com>
2023-10-09 21:03:03 -0700

    c basic

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - to print all single digit nos in base 10
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
+
