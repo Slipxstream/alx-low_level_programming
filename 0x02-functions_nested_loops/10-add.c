e1870aab93d15daba3c4d682153f2c8f92042d16
Babafemi Adesina <airchill101@gmail.com>
2023-10-11 20:12:36 -0700

    c basic

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - adds two ints
+ * @a: int to be added to b
+ * @b: int to be added to a
+ * Return: sum of both
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
+}
+
