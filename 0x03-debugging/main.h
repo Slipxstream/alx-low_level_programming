4e846dd313a196d9d16e98bdcf09aeb72c394342
Babafemi Adesina <airchill101@gmail.com>
2023-10-15 13:00:32 -0700

    c basic

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -6,6 +6,6 @@
 void positive_or_negative(int i);
 void print_remaining_days(int month, int day, int year);
 int largest_number(int a, int b, int c);
-int day_conversion(int month, int day);
+int convert_day(int month, int day);
 
 #endif /* MAIN_H */
