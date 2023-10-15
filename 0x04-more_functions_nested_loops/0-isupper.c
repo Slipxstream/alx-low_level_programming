c447c0a68ea2e3ecd40027b1b9305cf1b19ac7cf
Babafemi Adesina <airchill101@gmail.com>
2023-10-15 15:30:21 -0700

    c basic

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _is upper - detects characters in uppercase
+ *
+ * @c: input character
+ *
+ * Return: (0) false, (1) true
+ */
+
+int _isupper(int c)
+
+{
+	return (c >= 'A' && c <= 'Z');
+
+}
---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for digit
+ * @c: int to be checked
+ * Return: 1 for true, otherwise 0
+ */
+
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
