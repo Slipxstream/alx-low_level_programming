d18000c59a8304e28901287fac6bf680992ede1a
Babafemi Adesina <airchill101@gmail.com>
2023-10-15 11:48:22 -0700

    c basic

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+void print_days_left(int month, int day, int year);
+int largest_num(int a, int b, int c);
+int day_conversion(int month, int day);
+
+#endif /* MAIN_H */
