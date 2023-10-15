15375a339efc60a0af56837ac8fbffbd3594c247
Babafemi Adesina <airchill101@gmail.com>
2023-10-15 15:36:30 -0700

    c basic

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers 0 to 9 followed by a new line
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i < 10; i = 0; i++)
+	{
+		_putchar('0' + i);
+	}
+
+	_putchar('\n');
+}
