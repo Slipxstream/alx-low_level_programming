6315194a10634cea249f943c366f6b08fe70f7b4
Babafemi Adesina <airchill101@gmail.com>
2024-09-01 11:41:24 -0700

    0x14-bit_manipulation

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+*print_binary - prints the binary representation of a number
+*@n: number to be printed
+*
+*Return: void
+*/
+
+void print_binary(unsigned long int n)
+{
+	unsigned long int dev, result;
+	char flag;
+
+	flag = 0;
+	dev = _power(2, sizeof(unsigned long int) * 8 - 1);
+
+	while (dev != 0)
+	{
+		result = n & dev;
+		if (result == dev)
+		{
+			flag = 1;
+			_putchar('1');
+
+		}
+		else if (flag == 1 || dev == 1)
+		{
+			_putchar('0');
+		}
+		dev >>= 1;
+	}
+}
+
+/**
+ * _power - calculate (base and power)
+ * @base: ...
+ * @pow: ...
+ * Return: result
+ */
+unsigned long int _power(unsigned int base, unsigned int pow)
+{
+	unsigned long int num;
+	unsigned int i;
+
+	num = 1;
+	for (i = 1; i <= pow; i++)
+		num *= base;
+	return (num);
+}
